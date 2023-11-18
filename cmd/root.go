0d51a964389deb8721e00b8cffa83213484410ed
xelwarto <xelwarto.pub@gmail.com>
2023-11-18 10:03:12 -0600

    Added demo code

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,24 @@
 package cmd
 
 import (
+	"code/cmd/demo"
 	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
+// Root Command
 var rootCmd = &cobra.Command{
 	Use:   "aoc",
 	Short: "Advent of Code 2023",
 }
 
 func init() {
+	// Load demo commands for testing code
+	rootCmd.AddCommand(demo.DemoCmd)
+
+	// Load event day commands
 	rootCmd.AddCommand(day1Cmd)
 }
 
