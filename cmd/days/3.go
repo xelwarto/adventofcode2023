1b710d28ad74f0a935fcdaececb57ec21ed7d4d7
xelwarto <xelwarto.pub@gmail.com>
2023-12-01 09:01:18 -0600

    Setup day 3

--- cmd/days/3.go
+++ cmd/days/3.go
@@ -1,14 +1,41 @@
 package days
 
+import (
+	"code/util"
+	"fmt"
+)
+
 type Day3 int
 
 var day3 = Day3(3)
 
 func (d Day3) Part1() (string, error) {
+	s, err := util.File2Array("inputs/day3_part1.txt")
+	if err != nil {
+		return "", err
+	}
+
+	for _, x := range s {
+		if x != "" {
+			fmt.Println(x)
+		}
+	}
+
 	return "Day3", nil
 }
 
 func (d Day3) Part2() (string, error) {
+	s, err := util.File2Array("inputs/day3_part2.txt")
+	if err != nil {
+		return "", err
+	}
+
+	for _, x := range s {
+		if x != "" {
+			fmt.Println(x)
+		}
+	}
+
 	return "Day3", nil
 }
 
