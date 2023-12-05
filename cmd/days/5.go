663d422ffad4497e9c2c1b5b87632a5166052483
xelwarto <xelwarto.pub@gmail.com>
2023-12-05 17:03:40 -0600

    Day 5 updates

--- cmd/days/5.go
+++ cmd/days/5.go
@@ -15,6 +15,29 @@ var day5 = Day5(5)
 
 var seeds = []int{3037945983, 743948277, 2623786093, 391282324, 195281306, 62641412, 769611781, 377903357, 2392990228, 144218002, 1179463071, 45174621, 2129467491, 226193957, 1994898626, 92402726, 1555863421, 340215202, 426882817, 207194644}
 
+// func makeMap(s []string, sr int) int {
+// 	for _, x := range s {
+// 		if x != "" && !strings.Contains(x, "#") {
+// 			data := strings.Split(x, " ")
+
+// 			if len(data) == 3 {
+// 				dst, _ := strconv.Atoi(data[0])
+// 				src, _ := strconv.Atoi(data[1])
+// 				rng, _ := strconv.Atoi(data[2])
+
+// 				if sr >= src && sr <= (src+rng) {
+// 					for y := 0; y < rng; y++ {
+// 						if sr == (src + y) {
+// 							return dst + y
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return sr
+// }
+
 func makeMap(s []string, sr int) int {
 	for _, x := range s {
 		if x != "" && !strings.Contains(x, "#") {
@@ -26,11 +49,28 @@ func makeMap(s []string, sr int) int {
 				rng, _ := strconv.Atoi(data[2])
 
 				if sr >= src && sr <= (src+rng) {
-					for y := 0; y < rng; y++ {
-						if sr == (src + y) {
-							return dst + y
-						}
-					}
+					y := sr - src
+					return dst + y
+				}
+			}
+		}
+	}
+	return sr
+}
+
+func makeMap2(s []string, sr int) int {
+	for _, x := range s {
+		if x != "" && !strings.Contains(x, "#") {
+			data := strings.Split(x, " ")
+
+			if len(data) == 3 {
+				dst, _ := strconv.Atoi(data[0])
+				src, _ := strconv.Atoi(data[1])
+				rng, _ := strconv.Atoi(data[2])
+
+				if sr >= dst && sr <= (dst+rng) {
+					y := sr - dst
+					return src + y
 				}
 			}
 		}
@@ -78,6 +118,58 @@ func (d Day5) Part1() (string, error) {
 // ##################################################################################
 func (d Day5) Part2() (string, error) {
 	total := 0
+
+	s1, _ := util.File2Array("inputs/day5_1.txt")
+	s2, _ := util.File2Array("inputs/day5_2.txt")
+	s3, _ := util.File2Array("inputs/day5_3.txt")
+	s4, _ := util.File2Array("inputs/day5_4.txt")
+	s5, _ := util.File2Array("inputs/day5_5.txt")
+	s6, _ := util.File2Array("inputs/day5_6.txt")
+	s7, _ := util.File2Array("inputs/day5_7.txt")
+
+	for q := 0; q < 10000000000; q++ {
+		i7 := makeMap2(s7, q)
+		i6 := makeMap2(s6, i7)
+		i5 := makeMap2(s5, i6)
+		i4 := makeMap2(s4, i5)
+		i3 := makeMap2(s3, i4)
+		i2 := makeMap2(s2, i3)
+		i1 := makeMap2(s1, i2)
+
+		for w := 0; w < len(seeds); w = w + 2 {
+			if i1 >= seeds[w] && i1 < (seeds[w]+seeds[w+1]) {
+				if total == 0 {
+					total = q
+				} else {
+					if q < total {
+						total = q
+					}
+				}
+				fmt.Println(total)
+			}
+		}
+	}
+
+	// for q := 0; q < len(seeds); q = q + 2 {
+	// 	fmt.Println("HERE")
+	// 	for w := 0; w < seeds[q+1]; w++ {
+	// 		i1 := makeMap(s1, (seeds[q] + w))
+	// 		// i2 := makeMap(s2, i1)
+	// 		// i3 := makeMap(s3, i2)
+	// 		// i4 := makeMap(s4, i3)
+	// 		// i5 := makeMap(s5, i4)
+	// 		// i6 := makeMap(s6, i5)
+	// 		// i7 := makeMap(s7, i6)
+
+	// 		if total == 0 {
+	// 			total = i7
+	// 		} else {
+	// 			if i7 < total {
+	// 				total = i7
+	// 			}
+	// 		}
+	// 	}
+	// }
 
 	return fmt.Sprintf("%v", total), nil
 }
