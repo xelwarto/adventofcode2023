7feb21f46fddcabdf98232b54053f8ab0ad655fd
xelwarto <xelwarto.pub@gmail.com>
2023-12-15 10:40:25 -0600

    Day 15 complete

--- cmd/days/15.go
+++ cmd/days/15.go
@@ -3,6 +3,9 @@ package days
 import (
 	"code/util"
 	"fmt"
+	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 type Day15 int
@@ -18,11 +21,33 @@ func (d Day15) Part1() (string, error) {
 	total := 0
 	for _, x := range s {
 		if x != "" {
-			fmt.Println(x)
+			data := strings.Split(x, ",")
+			for _, d := range data {
+				cur := 0
+				code := strings.Split(d, "")
+				for _, c := range code {
+					r, _ := utf8.DecodeRuneInString(c)
+					cur = cur + int(r)
+					cur = cur * 17
+					cur = cur % 256
+				}
+				total = total + cur
+			}
 		}
 	}
-
 	return fmt.Sprintf("%v", total), nil
+}
+
+func Box(s string) int {
+	box := 0
+	code := strings.Split(s, "")
+	for _, c := range code {
+		r, _ := utf8.DecodeRuneInString(c)
+		box = box + int(r)
+		box = box * 17
+		box = box % 256
+	}
+	return box
 }
 
 func (d Day15) Part2() (string, error) {
@@ -32,9 +57,63 @@ func (d Day15) Part2() (string, error) {
 	}
 
 	total := 0
+	boxes := [][]map[string]int{}
+	for q := 0; q < 256; q++ {
+		boxes = append(boxes, []map[string]int{})
+	}
+
 	for _, x := range s {
 		if x != "" {
-			fmt.Println(x)
+			data := strings.Split(x, ",")
+			for _, d := range data {
+
+				if strings.Contains(d, "=") {
+					data2 := strings.Split(d, "=")
+					lens, _ := strconv.Atoi(data2[1])
+					label := make(map[string]int)
+					label[data2[0]] = lens
+					b := Box(data2[0])
+					box := boxes[b]
+
+					f := false
+					for q := 0; q < len(box); q++ {
+						b := box[q]
+						if b[data2[0]] != 0 {
+							b[data2[0]] = lens
+							f = true
+						}
+					}
+					if !f {
+						box = append(box, label)
+					}
+					boxes[b] = box
+
+				}
+
+				if strings.Contains(d, "-") {
+					data2 := strings.Split(d, "-")
+					b := Box(data2[0])
+					box := boxes[b]
+
+					nbox := []map[string]int{}
+					for q := 0; q < len(box); q++ {
+						b := box[q]
+						if b[data2[0]] == 0 {
+							nbox = append(nbox, b)
+						}
+					}
+					boxes[b] = nbox
+				}
+
+			}
+		}
+	}
+
+	for y, box := range boxes {
+		for q := 0; q < len(box); q++ {
+			for _, v := range box[q] {
+				total = total + ((y + 1) * (q + 1) * v)
+			}
 		}
 	}
 
