aaa97abed0d22495bea6e95e4aefa15231228328
xelwarto <xelwarto.pub@gmail.com>
2023-12-22 10:43:07 -0600

    Day 19 update

--- cmd/days/19.go
+++ cmd/days/19.go
@@ -178,6 +178,15 @@ func (d Day19) Part1() (string, error) {
 	return fmt.Sprintf("%v", total), nil
 }
 
+// ##############################################################################################
+// 167409079868000
+
+func Combos(w string) int {
+	val := 0
+
+	return val
+}
+
 func (d Day19) Part2() (string, error) {
 	s, err := util.File2Array("inputs/day19_part2.txt")
 	if err != nil {
@@ -185,11 +194,90 @@ func (d Day19) Part2() (string, error) {
 	}
 
 	total := 0
+
+	readParts := false
+	workflows = make(map[string][]Rule)
+	parts := []Part{}
 	for _, x := range s {
 		if x != "" {
-			fmt.Println(x)
+			if readParts {
+				x = strings.Replace(x, "{", "", -1)
+				x = strings.Replace(x, "}", "", -1)
+				data := strings.Split(x, ",")
+
+				p := Part{
+					Accepted: false,
+					Rejected: false,
+				}
+				for _, d := range data {
+					l := strings.Split(d, "=")
+					if len(l) == 2 {
+
+						if l[0] == "x" {
+							i, _ := strconv.Atoi(l[1])
+							p.X = i
+						}
+
+						if l[0] == "m" {
+							i, _ := strconv.Atoi(l[1])
+							p.M = i
+						}
+
+						if l[0] == "a" {
+							i, _ := strconv.Atoi(l[1])
+							p.A = i
+						}
+
+						if l[0] == "s" {
+							i, _ := strconv.Atoi(l[1])
+							p.S = i
+						}
+
+					}
+				}
+				parts = append(parts, p)
+			} else {
+				// rfg{s<537:gd,x>2440:R,A}
+				rules := []Rule{}
+				data := strings.Split(x, "{")
+				rs := strings.Replace(data[1], "}", "", -1)
+				rdata := strings.Split(rs, ",")
+				for _, r := range rdata {
+					rule := Rule{}
+
+					if strings.Contains(r, ":") {
+						data1 := strings.Split(r, ":")
+						rule.Workflow = data1[1]
+						if strings.Contains(data1[0], ">") {
+							rule.Operator = ">"
+							data2 := strings.Split(data1[0], ">")
+							i, _ := strconv.Atoi(data2[1])
+							rule.Value = i
+							rule.Part = data2[0]
+						}
+
+						if strings.Contains(data1[0], "<") {
+							rule.Operator = "<"
+							data2 := strings.Split(data1[0], "<")
+							i, _ := strconv.Atoi(data2[1])
+							rule.Value = i
+							rule.Part = data2[0]
+						}
+					} else {
+						rule.Workflow = r
+					}
+
+					rules = append(rules, rule)
+				}
+
+				workflows[data[0]] = rules
+			}
+		} else {
+			readParts = true
 		}
 	}
+
+	total = Combos("px")
 
 	return fmt.Sprintf("%v", total), nil
 }
